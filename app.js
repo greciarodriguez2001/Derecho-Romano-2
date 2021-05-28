cab1670d0d5e87d5d887e8fa531b0e5a064b8189
Luis Manuel Vivas <luis.manuel.vivas.r@gmail.com>
2021-05-28 19:29:38 -0400

    ventanas emergentes ready

---
+++ app.js
@@ -0,0 +1,89 @@
+//Pop Up intro 1
+
+let introBtn1 = document.getElementById("intro-btn1")
+let popup1 = document.getElementById("popup1")
+let closeBtn1 = document.getElementById("closeBtn1")
+
+
+introBtn1.onclick = () =>{
+    popup1.className = "popup-onclick"
+    }
+
+closeBtn1.onclick = () =>{
+        popup1.className = "popup"
+    }
+
+//Pop Up Intro 2
+
+let introBtn2 = document.getElementById("intro-btn2")
+let popup2 = document.getElementById("popup2")
+let closeBtn2 = document.getElementById("closeBtn2")
+
+
+introBtn2.onclick = () =>{
+        popup2.className = "popup-onclick"
+    }
+
+closeBtn2.onclick = () =>{
+        popup2.className = "popup"
+    }
+
+//Pop Up Info Romano 1
+
+let infoBtnRomano1 = document.getElementById("info-btn-romano-1")
+let popup3 = document.getElementById("popup3")
+let closeBtn3 = document.getElementById("closeBtn3")
+
+
+infoBtnRomano1.onclick = () =>{
+        popup3.className = "popup-onclick"
+    }
+
+closeBtn3.onclick = () =>{
+        popup3.className = "popup"
+    }
+
+//Pop Up Info Romano 2
+
+let infoBtnRomano2 = document.getElementById("info-btn-romano-2")
+let popup4 = document.getElementById("popup4")
+let closeBtn4 = document.getElementById("closeBtn4")
+
+
+infoBtnRomano2.onclick = () =>{
+        popup4.className = "popup-onclick"
+    }
+
+closeBtn4.onclick = () =>{
+        popup4.className = "popup"
+    }
+
+//Pop Up Info Actual 1
+
+let infoBtnActual1 = document.getElementById("info-btn-actual-1")
+let popup5 = document.getElementById("popup5")
+let closeBtn5 = document.getElementById("closeBtn5")
+
+
+infoBtnActual1.onclick = () =>{
+        popup5.className = "popup-onclick"
+    }
+
+closeBtn5.onclick = () =>{
+        popup5.className = "popup"
+    }
+
+//Pop Up Info Actual 2
+
+let infoBtnActual2 = document.getElementById("info-btn-actual-2")
+let popup6 = document.getElementById("popup6")
+let closeBtn6 = document.getElementById("closeBtn6")
+
+
+infoBtnActual2.onclick = () =>{
+        popup6.className = "popup-onclick"
+    }
+
+closeBtn6.onclick = () =>{
+        popup6.className = "popup"
+    }
